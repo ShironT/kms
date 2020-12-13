655795c05fc994f2610ca69f2c98a0e379294db1
Shiron <shironceylon@gmail.com>
2020-12-13 16:56:12 +0530

    Basic changes done *Added Nav and footer *Added basic pages *Basic routing done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import NavBar from "./components/layouts/navbar";
+import Home from "./components/pages/home";
+import Login from "./components/auth/login";
+import PostAProblem from "./components/pages/postAProblem";
+import ProblemList from "./components/pages/problemList";
+import ExpertList from "./components/pages/expertList";
+import Register from "./components/auth/register";
+import Footer from "./components/layouts/footer";
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return (
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/post-a-problem" component={PostAProblem} />
+            <Route path="/problem-list" component={ProblemList} />
+            <Route path="/expert-list" component={ExpertList} />
+          </Switch>
+          <Footer />
+        </div>
+      </Router>
+    );
   }
 }
 
